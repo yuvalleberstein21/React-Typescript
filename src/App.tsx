60745c5003b4d5a350fce0a3c2fa48f0d502bf48
yuvalleberstein21 <94398909+yuvalleberstein21@users.noreply.github.com>
2023-06-26 12:07:41 +0300

    todolist typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
 
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
 
-function App() {
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+  console.log(todos);
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <span className="heading">Taskify</span>
+
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
-}
+};
 
 export default App;
